a841cf953b44b64e5467fd7759ac7e9bd0ecb4b5
Casalnovo Giacomo <gcasalnovo@studenti.uninsubria.it>
2021-03-27 18:50:25 +0100

    Secondo Commit

---
+++ app/src/main/java/it/insubria/protezionet/MainActivity.kt
@@ -0,0 +1,22 @@
+package it.insubria.protezionet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    val TITOLO = "ProtezioNET"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        title = TITOLO
+
+        /// NUOVO PER COMMIT
+
+        // commit 2
+
+
+    }
+}
