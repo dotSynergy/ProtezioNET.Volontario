623ec395598b29fb01053e7b3daf809768cabba5
Astargisco <edubini@studenti.uninsubria.it>
2021-03-29 15:49:49 +0200

    -aggiunta dells Aplash activity che viene mostrata prima dell'apertura dell'applicazione -modificati/aggiunti i file AndroidManifest.xml, colors.xml, ic_launcher.png, ic_launcher_background.xml, ic_launcher_round.png, LoginActivity.kt, themes.xml
    Aggiornamento: nella classe SplashActivity.kt è stata aggiunta la documentazione e inserito un sistema che permette di visualizzare lo spalsh screen all'avvio dell'app per 3 secondi

--- app/src/main/java/it/insubria/protezionet/admin/SplashActivity.kt
+++ app/src/main/java/it/insubria/protezionet/admin/SplashActivity.kt
@@ -3,14 +3,30 @@ package it.insubria.protezionet.admin
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import androidx.core.os.HandlerCompat.postDelayed
 import it.insubria.protezionet.LoginActivity
 import it.insubria.protezionet.R
 
+/**
+ * Activity utilizzata per la visualizzazione dello splash screen prima di entrare nell'activity di login
+ *
+ * per ulteriori informazioni sulla realizzazione visionare i seguenti link:
+ *  https://italiancoders.it/splash-screen-in-android-impariamo-a-crearle-nel-modo-corretto/
+ *  https://informaticabrutta.it/launch-splash-screen-android/
+ */
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        startActivity(Intent(this, LoginActivity::class.java))
-        finish()
+        Handler().postDelayed(  //todo Handler è deprecato ma non saprei cosa utilizzare altrimenti
+                {
+                    // Terminati i 3 secondi di attesa verrà lanciata l'activity
+                    // principale e terminata quella di launch
+                    startActivity(Intent(this, LoginActivity::class.java))
+                    finish()
+                },
+                3000 // La Launch Screen rimarrà visibile per 3 secondi
+        )
     }
 }
