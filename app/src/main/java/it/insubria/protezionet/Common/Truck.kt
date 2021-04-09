d34d89df0fe59e0b05a59a0500eeacac3adf60b8
Casalnovo Giacomo <gcasalnovo@studenti.uninsubria.it>
2021-04-09 17:51:16 +0200

    Class Truck base

--- app/src/main/java/it/insubria/protezionet/Common/Truck.kt
+++ app/src/main/java/it/insubria/protezionet/Common/Truck.kt
@@ -1,4 +1,12 @@
 package it.insubria.protezionet.Common
 
 data class Truck(val tipo: String, val targa: String) {
+
+    //  ADMIN
+    fun addTruck(){}
+
+    fun deleteTruck(){}
+
+    // USER
+
 }
